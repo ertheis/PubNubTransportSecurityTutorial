c38cea5ece0e491f6d903f224c32db33b3ecb729
ertheis <ertheis@Erics-MacBook-Pro.local>
2014-07-09 11:02:52 -0700

    Initial Commit

---
+++ PubNubTransportSecurityTutorial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PubNubTransportSecurityTutorial
+//
+//  Created by Eric Theis on 7/9/14.
+//  Copyright (c) 2014 PubNub. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
